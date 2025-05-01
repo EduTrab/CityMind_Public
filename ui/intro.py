02baee6aa8ca48482eeca81b9a167e3c8a203a5f
Eduardo Trabattoni <trabae@usi.ch>
2025-05-01 14:24:05 +0200

    impruved info text

--- ui/intro.py
+++ ui/intro.py
@@ -5,11 +5,13 @@ def render_intro():
     st.markdown(
         """
         **Instructions**:
-        1. Choose a batch size and LLM model from the sidebar.
+        1. Choose a batch size from the sidebar.
         2. Select a dataset source:
            - **Default Dataset**: Downloads images from Street View.
            - **Local Dataset**: Upload your own images.
+           - **City Dataset**: Choose the City you want to analyse.
         3. Click **LLM closed Q&A** to process images.
         4. Review (and optionally refine) the generated MCQs, then click **Submit All Answers**.
+        5. **IMPORTANT** : At least 1 image per batch needs to be selected as **not relevant**.
         """
     )
